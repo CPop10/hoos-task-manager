f71c0620ebd1db13561097caea140be84095a871
CPop10 <cpopielec10@gmail.com>
2024-10-19 11:02:13 -0400

    adding Spring Boot

---
+++ demo/src/main/java/GirlsHooHack/demo/GirlsHooHackHackathonApplication.java
@@ -0,0 +1,13 @@
+package GirlsHooHack.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GirlsHooHackHackathonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GirlsHooHackHackathonApplication.class, args);
+	}
+
+}
---
+++ demo/src/test/java/GirlsHooHack/demo/GirlsHooHackHackathonApplicationTests.java
@@ -0,0 +1,13 @@
+package GirlsHooHack.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GirlsHooHackHackathonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
