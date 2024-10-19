d4738e92fd6f59b93fee37f187168f44ee702ad8
CPop10 <cpopielec10@gmail.com>
2024-10-19 11:55:16 -0400

    created packages, installed maven

---
+++ girlshoohack/src/main/java/uva/girlshoohack/service/SendEmailService.java
@@ -0,0 +1,16 @@
+package uva.girlshoohack.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
+
+@Service
+public class SendEmailService {
+
+    @Autowired
+    private JavaMailSender javaMailSender;
+
+    public void sendEmail(){
+
+    }
+}
